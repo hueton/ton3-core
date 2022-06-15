9bf7cae5e27daac5f975185c03f54a1a26c97fd1
Anton Spivak <oboupo@gmail.com>
2022-06-15 15:58:17 +0300

    Add methods for v2 and v4 wallets

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,45 @@ import {
 } from './boc'
 
 //
+// Wallet2
+//
+
+const createWallet2InitialData = function(publicKey: Uint8Array): string {
+    const cell = new Builder()
+        .storeUint(0, 32)
+        .storeBytes(publicKey)
+        .cell();
+    return BOC.toHexStandard(cell);
+};
+
+const createWallet2TransferMessageBody = function(seqno: number, timeout: number, bounceable: boolean, address: Uint8Array, workchain: number, amount: number, message: string | null): string {
+    const body = new Builder()
+        .storeUint(timeout, 32) // valid until
+        .storeUint(seqno, 32)
+
+    const internalMessageBody = new Builder()
+        .storeUint(0, 32)
+    
+    if (message !== null) {
+        internalMessageBody.storeString(message)
+    }
+
+    const internalMessage = new MessageInternal({
+        bounce: bounceable,
+        srcAddress: null,
+        destAddress: address,
+        destWorkchain: workchain,
+        value: amount
+    }, internalMessageBody.cell())
+
+    // 3: default message send mode
+    body.storeUint(3, 8)
+        .storeRef(internalMessage.cell())
+
+    return BOC.toHexStandard(body.cell());
+};
+
+//
 // Wallet3
 //
 
@@ -18,22 +57,54 @@ const createWallet3InitialData = function(subwalletID: number, publicKey: Uint8A
     return BOC.toHexStandard(cell);
 };
 
-const createWallet3DeployMessageBody = function(subwalletID: number): string {
-    const body = new Builder()
-        .storeUint(subwalletID, 32)
-        .storeInt(-1, 32) // valid until
-        .storeUint(0, 32) // seqno
-        .cell()
-
-    return BOC.toHexStandard(body);
-};
-
-// timeout in seconds
 const createWallet3TransferMessageBody = function(subwalletID: number, seqno: number, timeout: number, bounceable: boolean, address: Uint8Array, workchain: number, amount: number, message: string | null): string {
     const body = new Builder()
         .storeUint(subwalletID, 32)
         .storeUint(timeout, 32) // valid until
         .storeUint(seqno, 32)
+
+    const internalMessageBody = new Builder()
+        .storeUint(0, 32)
+    
+    if (message !== null) {
+        internalMessageBody.storeString(message)
+    }
+
+    const internalMessage = new MessageInternal({
+        bounce: bounceable,
+        srcAddress: null,
+        destAddress: address,
+        destWorkchain: workchain,
+        value: amount
+    }, internalMessageBody.cell())
+
+    // 3: default message send mode
+    body.storeUint(3, 8)
+        .storeRef(internalMessage.cell())
+
+    return BOC.toHexStandard(body.cell());
+};
+
+//
+// Wallet4
+//
+
+const createWallet4InitialData = function(subwalletID: number, publicKey: Uint8Array): string {
+    const cell = new Builder()
+        .storeUint(0, 32)
+        .storeUint(subwalletID, 32)
+        .storeBytes(publicKey)
+        .storeBit(0)
+        .cell();
+    return BOC.toHexStandard(cell);
+};
+
+const createWallet4TransferMessageBody = function(subwalletID: number, seqno: number, timeout: number, bounceable: boolean, address: Uint8Array, workchain: number, amount: number, message: string | null): string {
+    const body = new Builder()
+        .storeUint(subwalletID, 32)
+        .storeUint(timeout, 32) // valid until
+        .storeUint(seqno, 32)
+        .storeUint(0, 8) // op
 
     const internalMessageBody = new Builder()
         .storeUint(0, 32)
@@ -81,9 +152,14 @@ const getBOCRootCellData = function(boc: string): string {
 }
 
 export { 
+    createWallet2InitialData,
+    createWallet2TransferMessageBody,
+
     createWallet3InitialData,
-    createWallet3DeployMessageBody,
     createWallet3TransferMessageBody,
+
+    createWallet4InitialData,
+    createWallet4TransferMessageBody,
 
     createBOCHash,
     createBOCWithSignature,
